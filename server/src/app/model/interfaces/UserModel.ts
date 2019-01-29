3b2d69d5ce68d9208ef68bb13ef613e2abfc33e4
rapidera <rajashree08umrekar@gmail.com>
2019-01-29 19:27:56 +0530

    "SSL and Node mailer implementation"

--- server/src/app/model/interfaces/UserModel.ts
+++ server/src/app/model/interfaces/UserModel.ts
@@ -1,15 +1,16 @@
 import mongoose = require('mongoose');
 
-interface UserModel extends mongoose.Document{
+interface UserModel extends mongoose.Document {
 
     _id?: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    token:string;
-    salt:string;
-
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    password?: string;
+    token?: string;
+    salt?: string;
+    reset_password_token?: string;
+    reset_password_expires?: number;
 }
 
 export = UserModel;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,22 @@
 import express = require('express');
+import fs = require('fs');
+import https = require('https');
+import path = require('path');
+
+
 require('dotenv').config();
 
 import Middlewares = require('./config/middlewares/base/');
+
+const key = fs.readFileSync(path.resolve('/home/rapidera/RaulD_Workspace/MEAN/quora-clone/server/src/config/ssl/key.pem'));
+const certificate = fs.readFileSync(path.resolve('/home/rapidera/RaulD_Workspace/MEAN/quora-clone/server/src/config/ssl/server.crt'));
+const options = {
+    key :key,
+    certificate : certificate
+};
+
+
+console.log(options);
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 5000;
@@ -11,3 +26,7 @@ app.use(Middlewares.configuration);
 app.listen(port, () => {
     console.log("Node app is running at localhost:" + port);
 })
+
+// https.createServer(options, app).listen(port,() => {
+//         console.log("Node app is running at localhost:" + port);
+//     });
